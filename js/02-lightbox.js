9b60845c3327fab22978905aade753c80a57dd33
Roman <140334590+RomanS1994@users.noreply.github.com>
2023-12-23 17:45:32 +0100

    library connection

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+console.log(galleryList);
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        ` <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>`
+    )
+    .join("");
+}
+galleryList.insertAdjacentHTML("afterbegin", createMarkup(galleryItems));
+console.log(cloudflare);
